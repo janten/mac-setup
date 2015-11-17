af0280d65b9488ba631578c477fbf2cd8e8321ba
Jan-Gerd Tenberge <janten@gmail.com>
2015-11-17 01:03:36 +0100

    Add more output

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ command_exists () {
 }
 
 if ! xcode-select -p &> /dev/null ; then
+	echo "Installing Command Line Tools"
 	touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 	PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
 	softwareupdate -i "$PROD" -v
@@ -13,52 +14,61 @@ else
 	return 0
 fi
 
-git clone https://github.com/janten/mac-setup.git /tmp/mac-setup
-cd /tmp/mac-setup
-
-cp zshrc ~/.zshrc
-cp gitconfig ~/.gitconfig
-cp gitignore_global ~/.gitignore_global
-cp com.apple.Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
-cp "Inconsolata for Powerline.otf" ~/Library/Fonts/
-
 if ! command_exists brew ; then
+	echo "Installing Homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
 	echo "Homebrew already installed"
 fi
 
 if ! command_exists brew-cask ; then
+	echo "Installing brew-cask"
     brew install caskroom/cask/brew-cask
 else
 	echo "brew-cask already installed"
 fi
 
 if ! command_exists mate ; then
+	echo "Installing TextMate"
 	brew cask install textmate
 else
 	echo "TextMate already installed"
 fi
 
 if ! command_exists wget ; then
+	echo "Installing wget"
     brew install wget
 else
 	echo "wget already installed"
 fi
 
 if ! command_exists upgrade_oh_my_zsh ; then
+	echo "Installing oh-my-zsh"
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
 	upgrade_oh_my_zsh
 fi
 
 if ! brew list zsh-syntax-highlighting &> /dev/null ; then
+	echo "Installing zsh-syntax-highlighting"
 	brew install zsh-syntax-highlighting
 else
-	echo "zsh completion already installed"
+	echo "zsh-syntax-highlighting already installed"
 fi
 
+echo "Cloning mac-setup"
+git clone https://github.com/janten/mac-setup.git /tmp/mac-setup
+
+echo "Setting preferences"
+cp /tmp/mac-setup/zshrc ~/.zshrc
+cp /tmp/mac-setup/gitconfig ~/.gitconfig
+cp /tmp/mac-setup/gitignore_global ~/.gitignore_global
+cp /tmp/mac-setup/com.apple.Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
+cp "/tmp/mac-setup/Inconsolata for Powerline.otf" ~/Library/Fonts/
+
+echo "Cleaning up"
 brew cleanup
 brew cask cleanup
+rm -rf /tmp/mac-setup
 
 echo "Installation complete. You should restart your terminal."
