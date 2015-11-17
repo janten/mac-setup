aa45d33c3d39242a194e9b6c86ec7940b267618a
Jan-Gerd Tenberge <janten@gmail.com>
2015-11-17 01:08:33 +0100

    Remove softwareupdate output

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if ! xcode-select -p &> /dev/null ; then
 	echo "Installing Command Line Tools"
 	touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 	PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
-	softwareupdate -i "$PROD" -v
+	softwareupdate -i "$PROD"
 else
 	echo "Command Line Tools already installed"
 	return 0
