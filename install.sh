27cd4c1f926da03a9f36403f2f53917f9af5d134
Jan-Gerd Tenberge <janten@gmail.com>
2016-01-13 18:46:31 +0100

    Adding time and date setup and software update

--- install.sh
+++ install.sh
@@ -4,7 +4,15 @@ command_exists () {
     type "$1" &> /dev/null ;
 }
 
-if ! xcode-select -p &> /dev/null ; then
+echo "Setting time and date"
+sudo ntpdate -u time.uni-muenster.de
+sudo ntpdate -u 1.de.pool.ntp.org
+
+echo "Updating software"
+softwareupdate --install --all
+
+if ! xcode-select -p &> /dev/null
+then
 	echo "Installing Command Line Tools"
 	touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 	PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
@@ -13,7 +21,8 @@ else
 	echo "Command Line Tools already installed"
 fi
 
-if ! command_exists brew ; then
+if ! command_exists brew
+then
 	echo "Installing Homebrew"
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
@@ -24,35 +33,53 @@ echo "Updating Homebrew"
 brew update
 brew upgrade
 
-if ! command_exists brew-cask ; then
+if ! command_exists brew-cask
+then
 	echo "Installing brew-cask"
 	brew install caskroom/cask/brew-cask
 else
 	echo "brew-cask already installed"
 fi
 
-if ! command_exists mate ; then
+echo "Updating brew-cask"
+brew cask update
+brew cask upgrade
+
+if ! command_exists mate
+then
 	echo "Installing TextMate"
 	brew cask install textmate
 else
 	echo "TextMate already installed"
 fi
 
-if ! command_exists wget ; then
+if ! command_exists wget
+then
 	echo "Installing wget"
 	brew install wget
 else
 	echo "wget already installed"
 fi
 
-if ! command_exists upgrade_oh_my_zsh ; then
+if [ ! -f ~/.ssh/authorized_keys ]
+then
+	echo "Installing SSH keys from GitHub"
+	mkdir ~/.ssh
+	curl "https://github.com/janten.keys" > ~/.ssh/authorized_keys
+else
+	echo "Some SSH keys already present"
+fi
+
+if ! command_exists upgrade_oh_my_zsh
+then
 	echo "Installing oh-my-zsh"
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 else
 	upgrade_oh_my_zsh
 fi
 
-if ! brew list zsh-syntax-highlighting &> /dev/null ; then
+if ! brew list zsh-syntax-highlighting &> /dev/null
+then
 	echo "Installing zsh-syntax-highlighting"
 	brew install zsh-syntax-highlighting
 else
